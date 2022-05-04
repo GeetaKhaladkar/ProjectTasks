37d18ba52d1bd28a04192bdcf406ab298fd8bc07
GeetaKhaladkar <95268074+GeetaKhaladkar@users.noreply.github.com>
2022-05-04 16:27:01 +0530

    Update credentials.tf

--- RDS_1/credentials.tf
+++ RDS_1/credentials.tf
@@ -2,6 +2,14 @@ variable "username"{
 	default		="admin"
 }
 
-variable "password"{
-	default		="geeta1234"
+variable "create_random_password" {
+  description = "Determines whether to create random password for RDS primary cluster"
+  type        = bool
+  default     = true
+}
+
+variable "random_password_length" {
+  description = "Length of random password to create. Defaults to `10`"
+  type        = number
+  default     = 10
 }
