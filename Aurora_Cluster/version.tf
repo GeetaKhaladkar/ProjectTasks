4d753cec8d03440d8b31e04e0300141c025541f9
GeetaKhaladkar <95268074+GeetaKhaladkar@users.noreply.github.com>
2022-05-04 16:43:05 +0530

    Add files via upload

---
+++ Aurora_Cluster/version.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.12"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 2.0"
+    }
+
+  }
+}
