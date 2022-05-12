b8df01ade58f6a9c11c9ecb9f2ca56bcee553c6c
GeetaKhaladkar <95268074+GeetaKhaladkar@users.noreply.github.com>
2022-05-12 15:55:38 +0530

    Update output.tf

--- Aurora_Cluster/output.tf
+++ Aurora_Cluster/output.tf
@@ -1,70 +1,71 @@
-output "database_name" {
-  value       = var.db_name
-  description = "Database name"
+#####
+# aws_rds_cluster
+#####
+output "rds_cluster_arn" {
+  description = "The ID of the aurora cluster"
+  value       = var.enable_global_cluster ? join("", aws_rds_cluster.global.*.arn) : join("", aws_rds_cluster.main.*.arn)
 }
 
-output "master_username" {
-  value       = local.is_regional_cluster ? join("", aws_rds_cluster.primary.*.master_username) : join("", aws_rds_cluster.secondary.*.master_username)
-  description = "Username for the master DB user"
+output "rds_cluster_id" {
+  description = "The ID of the cluster"
+  value       = var.enable_global_cluster ? join("", aws_rds_cluster.global.*.id) : join("", aws_rds_cluster.main.*.id)
+}
+
+output "rds_cluster_resource_id" {
+  description = "The Resource ID of the cluster"
+  value       = var.enable_global_cluster ? join("", aws_rds_cluster.global.*.cluster_resource_id) : join("", aws_rds_cluster.main.*.cluster_resource_id)
+}
+
+output "rds_cluster_endpoint" {
+  description = "The cluster endpoint"
+  value       = var.enable_global_cluster ? join("", aws_rds_cluster.global.*.endpoint) : join("", aws_rds_cluster.main.*.endpoint)
+}
+
+output "rds_cluster_reader_endpoint" {
+  description = "The cluster reader endpoint"
+  value       = var.enable_global_cluster ? join("", aws_rds_cluster.global.*.reader_endpoint) : join("", aws_rds_cluster.main.*.reader_endpoint)
+}
+
+output "rds_cluster_master_password" {
+  description = "The master password"
+  value       = var.enable_global_cluster ? aws_rds_cluster.global.*.master_password : aws_rds_cluster.main.*.master_password
   sensitive   = true
 }
 
-output "cluster_identifier" {
-  value       = local.is_regional_cluster ? join("", aws_rds_cluster.primary.*.cluster_identifier) : join("", aws_rds_cluster.secondary.*.cluster_identifier)
-  description = "Cluster Identifier"
+output "rds_cluster_port" {
+  description = "The port"
+  value       = var.enable_global_cluster ? join("", aws_rds_cluster.global.*.port) : join("", aws_rds_cluster.main.*.port)
 }
 
-output "arn" {
-  value       = local.is_regional_cluster ? join("", aws_rds_cluster.primary.*.arn) : join("", aws_rds_cluster.secondary.*.arn)
-  description = "Amazon Resource Name (ARN) of the cluster"
+output "rds_cluster_master_username" {
+  description = "The master username"
+  value       = var.enable_global_cluster ? join("", aws_rds_cluster.global.*.master_username) : join("", aws_rds_cluster.main.*.master_username)
 }
 
-output "endpoint" {
-  value       = local.is_regional_cluster ? join("", aws_rds_cluster.primary.*.endpoint) : join("", aws_rds_cluster.secondary.*.endpoint)
-  description = "The DNS address of the RDS instance"
+#####
+# aws_rds_cluster_instance
+#####
+output "rds_cluster_instance_endpoints" {
+  description = "A list of all cluster instance endpoints"
+  value       = aws_rds_cluster_instance.main.*.endpoint
 }
 
-output "reader_endpoint" {
-  value       = local.is_regional_cluster ? join("", aws_rds_cluster.primary.*.reader_endpoint) : join("", aws_rds_cluster.secondary.*.reader_endpoint)
-  description = "A read-only endpoint for the Aurora cluster, automatically load-balanced across replicas"
+output "rds_cluster_instance_arns" {
+  description = "A list of all cluster instance ARNs"
+  value       = aws_rds_cluster_instance.main.*.arn
 }
 
-output "master_host" {
-  value       = module.dns_master.hostname
-  description = "DB Master hostname"
+output "rds_cluster_instance_ids" {
+  description = "A list of all cluster instance ids"
+  value       = aws_rds_cluster_instance.main.*.id
 }
 
-output "replicas_host" {
-  value       = module.dns_replicas.hostname
-  description = "Replicas hostname"
-}
-
-output "dbi_resource_ids" {
-  value       = aws_rds_cluster_instance.default.*.dbi_resource_id
-  description = "List of the region-unique, immutable identifiers for the DB instances in the cluster"
-}
-
-output "cluster_resource_id" {
-  value       = local.is_regional_cluster ? join("", aws_rds_cluster.primary.*.cluster_resource_id) : join("", aws_rds_cluster.secondary.*.cluster_resource_id)
-  description = "The region-unique, immutable identifie of the cluster"
-}
-
-output "cluster_security_groups" {
-  value       = coalescelist(aws_rds_cluster.primary.*.vpc_security_group_ids, aws_rds_cluster.secondary.*.vpc_security_group_ids, [""])
-  description = "Default RDS cluster security groups"
+output "rds_cluster_instance_dbi_resource_ids" {
+  description = "A list of all the region-unique, immutable identifiers for the DB instances"
+  value       = aws_rds_cluster_instance.main.*.dbi_resource_id
 }
 
 output "security_group_id" {
-  value       = join("", aws_security_group.default.*.id)
-  description = "Security Group ID"
-}
-
-output "security_group_arn" {
-  value       = join("", aws_security_group.default.*.arn)
-  description = "Security Group ARN"
-}
-
-output "security_group_name" {
-  value       = join("", aws_security_group.default.*.name)
-  description = "Security Group name"
+  description = "The security group ID of the cluster"
+  value       = join("", aws_security_group.main.*.id)
 }
