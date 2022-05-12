c100edbff71cf4e087b75c5766e9c7c1c362a7b4
GeetaKhaladkar <95268074+GeetaKhaladkar@users.noreply.github.com>
2022-05-12 15:55:07 +0530

    Update version.tf

--- Aurora_Cluster/version.tf
+++ Aurora_Cluster/version.tf
@@ -1,16 +1,8 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 0.13.0"
 
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.12"
-    }
-
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 2.0"
-    }
-
+    aws    = ">= 3.15"
+    random = ">= 2.3"
   }
 }
