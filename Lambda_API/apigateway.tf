a2bb00a7550cef14a47edae0a4e29037c50acd31
GeetaKhaladkar <95268074+GeetaKhaladkar@users.noreply.github.com>
2022-05-17 15:50:04 +0530

    Update apigateway.tf

--- Lambda_API/apigateway.tf
+++ Lambda_API/apigateway.tf
@@ -1,29 +1,29 @@
-resource "aws_api_gateway_rest_api" "examplepy" {
-  name        = "Serverlessexamplepy"
+resource "aws_api_gateway_rest_api" "terraform_lambda_func" {
+  name        = "Serverlessterraform_lambda_func"
   description = "Terraform Serverless Application Example python"
 }
 
 resource "aws_api_gateway_resource" "number" {
-  parent_id   = aws_api_gateway_rest_api.examplepy.root_resource_id
-  rest_api_id = aws_api_gateway_rest_api.examplepy.id
+  parent_id   = aws_api_gateway_rest_api.terraform_lambda_func.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.terraform_lambda_func.id
   path_part   = "{${var.resource_name}+}"
 }
 
 resource "aws_api_gateway_method" "number" {
-   rest_api_id   = aws_api_gateway_rest_api.examplepy.id
+   rest_api_id   = aws_api_gateway_rest_api.terraform_lambda_func.id
    resource_id   = aws_api_gateway_resource.number.id
    http_method   = "GET"
    authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambdapy" {
-   rest_api_id = aws_api_gateway_rest_api.examplepy.id
+   rest_api_id = aws_api_gateway_rest_api.terraform_lambda_func.id
    resource_id = aws_api_gateway_method.number.resource_id
    http_method = aws_api_gateway_method.number.http_method
 
    integration_http_method = "POST"
    type                    = "AWS"
-   uri                     = aws_lambda_function.examplepy.invoke_arn
+   uri                     = aws_lambda_function.terraform_lambda_func.invoke_arn
    passthrough_behavior = "WHEN_NO_TEMPLATES"
    
    request_templates = {
@@ -36,20 +36,20 @@ EOF
 }
 
 resource "aws_api_gateway_method" "number_rootpy" {
-   rest_api_id   = aws_api_gateway_rest_api.examplepy.id
-   resource_id   = aws_api_gateway_rest_api.examplepy.root_resource_id
+   rest_api_id   = aws_api_gateway_rest_api.terraform_lambda_func.id
+   resource_id   = aws_api_gateway_rest_api.terraform_lambda_func.root_resource_id
    http_method   = "GET"
    authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda_rootpy" {
-   rest_api_id = aws_api_gateway_rest_api.examplepy.id
+   rest_api_id = aws_api_gateway_rest_api.terraform_lambda_func.id
    resource_id = aws_api_gateway_method.number_rootpy.resource_id
    http_method = aws_api_gateway_method.number_rootpy.http_method
 
    integration_http_method = "POST"
    type                    = "AWS"
-   uri                     = aws_lambda_function.examplepy.invoke_arn
+   uri                     = aws_lambda_function.terraform_lambda_func.invoke_arn
 #   passthrough_behavior = "WHEN_NO_TEMPLATES"
 #   request_templates = {
 #    "application/json" = <<EOF
@@ -59,7 +59,7 @@ resource "aws_api_gateway_integration" "lambda_rootpy" {
 }
 
 resource "aws_api_gateway_method_response" "response_200" {
- rest_api_id = aws_api_gateway_rest_api.examplepy.id
+ rest_api_id = aws_api_gateway_rest_api.terraform_lambda_func.id
  resource_id = aws_api_gateway_resource.number.id
  http_method = aws_api_gateway_method.number.http_method
  status_code = "200"
@@ -72,7 +72,7 @@ resource "aws_api_gateway_integration_response" "IntegrationResponse" {
      aws_api_gateway_integration.lambdapy,
      aws_api_gateway_integration.lambda_rootpy,
   ]
-  rest_api_id = aws_api_gateway_rest_api.examplepy.id
+  rest_api_id = aws_api_gateway_rest_api.terraform_lambda_func.id
   resource_id = aws_api_gateway_resource.number.id
   http_method = aws_api_gateway_method.number.http_method
   status_code = aws_api_gateway_method_response.response_200.status_code
@@ -86,7 +86,7 @@ resource "aws_api_gateway_integration_response" "IntegrationResponse" {
 
 # Model
 # resource "aws_api_gateway_model" "MyDemoModel" {
-# rest_api_id = "${aws_api_gateway_rest_api.examplepy.id}"
+# rest_api_id = "${aws_api_gateway_rest_api.terraform_lambda_func.id}"
 # name = "usermodel"
 # description = "a JSON schema"
 # content_type = "application/json"
@@ -106,17 +106,17 @@ resource "aws_api_gateway_integration_response" "IntegrationResponse" {
 # EOF
 #}
 
-resource "aws_api_gateway_deployment" "examplepy" {
+resource "aws_api_gateway_deployment" "terraform_lambda_func" {
    depends_on = [
      aws_api_gateway_integration.lambdapy,
      aws_api_gateway_integration_response.IntegrationResponse,
    ]
 
-   rest_api_id = aws_api_gateway_rest_api.examplepy.id
+   rest_api_id = aws_api_gateway_rest_api.terraform_lambda_func.id
    stage_name  = var.stage
 
 }
 
 output "base_url" {
-  value = "${aws_api_gateway_deployment.examplepy.invoke_url}/${var.resource_name}"
+  value = "${aws_api_gateway_deployment.terraform_lambda_func.invoke_url}/${var.resource_name}"
 }
