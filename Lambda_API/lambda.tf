6cc7358d35e26114fb6f1d5c570dbf5cc6404edc
GeetaKhaladkar <95268074+GeetaKhaladkar@users.noreply.github.com>
2022-05-17 15:50:31 +0530

    Update lambda.tf

--- Lambda_API/lambda.tf
+++ Lambda_API/lambda.tf
@@ -1,57 +1,22 @@
-
-resource "aws_lambda_function" "examplepy" {
-   function_name = "Serverlessexamplepy"
-
-   # The S3 bucket should already exists
-
-   s3_bucket = "bogo-terraform-serverless-examplepy"
-   s3_key    = "v${var.app_version}/examplepy.zip"   
-   handler = "lambda_function.lambda_handler"
-   runtime = "python3.8"
-   role = aws_iam_role.lambda_execpy.arn
-   depends_on = [aws_iam_role_policy_attachment.lambda_execpy]
-
+data "archive_file" "zip_the_python_code" {
+type        = "zip"
+source_dir  = "${path.module}/python/"
+output_path = "${path.module}/python/hello-python.zip"
 }
 
- # IAM role which dictates what other AWS services the Lambda function may access.
-
-  resource "aws_iam_role" "lambda_execpy" {
-
-   name = "serverless_example_lambdapy"
-
-   assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
+resource "aws_lambda_function" "terraform_lambda_func" {
+filename                       = "${path.module}/python/hello-python.zip"
+function_name                  = "Test_Lambda_Function"
+role                           = aws_iam_role.lambda_role.arn
+handler                        = "index.lambda_handler"
+runtime                        = "python3.8"
+depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
-EOF
-
-}
-
-
-resource "aws_iam_role_policy_attachment" "lambda_execpy" {
-  role       = aws_iam_role.lambda_execpy.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
 
 resource "aws_lambda_permission" "apigw" {
    statement_id  = "AllowAPIGatewayInvoke"
    action        = "lambda:InvokeFunction"
-   function_name = aws_lambda_function.examplepy.function_name
+   function_name = aws_lambda_function.terraform_lambda_func.function_name
    principal     = "apigateway.amazonaws.com"
-
-   # The "/*/*" portion grants access from any method on any resource
-   # within the API Gateway REST API.
-
-   source_arn = "${aws_api_gateway_rest_api.examplepy.execution_arn}/*/*"
+   source_arn = "${aws_api_gateway_rest_api.terraform_lambda_func.execution_arn}/*/*"
 }
